3049026b4d1e86b8da7f6c4cd709d126860915b1
AlpenJodler <dany.meyer@hnu.de>
2023-09-06 15:57:50 +0200

    add files

---
+++ NodeMCU_arduino_ide_speedoMeter/NodeMCU_arduino_ide_speedoMeter.ino
@@ -0,0 +1,159 @@
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+#include <ArduinoMqttClient.h>
+#include <TinyGPS++.h>
+#include <ArduinoJson.h>
+#include <string.h>
+#include <SoftwareSerial.h>
+
+TinyGPSPlus gps;
+/* TX --> D2, RX --> D1*/
+SoftwareSerial SerialGPS(4,5); 
+
+bool debug = true;
+WiFiManager wifiManager;
+WiFiClient wifiClient;
+
+MqttClient mqttClient(wifiClient);
+
+
+const char broker[] = "broker.hivemq.com"; //"test.mosquitto.org";
+int        port     = 1883;
+String topic_str = "ima_test";
+
+const long interval = 5000;
+unsigned long previousMillis = 0;
+
+static byte mymac[] = { 0x00,0x00,0x00,0x00,0x00,0x00 };
+String myMacAdr = "";
+
+void wifi_reconnect() {
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.println("Connection lost");
+    while (WiFi.status() != WL_CONNECTED){
+      uint8_t timeout = 8;
+      while (timeout && (WiFi.status() != WL_CONNECTED)) {
+        timeout--;
+        if (debug) Serial.print(".");
+        delay(1000);
+      }
+    }
+    if (debug) Serial.println("Wifi connected");
+  }
+  if (!mqttClient.connected()) {
+    while(!mqttClient.connected()){
+      mqttClient.connect(broker, port);
+      uint8_t timeout = 8;
+      while (timeout && (!mqttClient.connected())){
+        timeout--;
+        if (debug) Serial.print(".");
+        delay(1000);
+      }
+    }
+    if (debug) Serial.println("MQTT Broker connected");
+  }
+}
+
+
+void setup() {
+  if (debug) {
+    Serial.begin(115200);
+    while (!Serial) {;}
+    Serial.println("Start Program");
+  }  
+  
+  WiFi.macAddress(mymac);
+  for (byte i = 0; i < 6; ++i) {
+    myMacAdr = myMacAdr + String(mymac[i], HEX);
+    if (i < 5) myMacAdr = myMacAdr + '-';
+  }
+  if (debug) {Serial.println(myMacAdr);}
+
+  wifiManager.setConfigPortalTimeout(180);
+  if(!wifiManager.autoConnect("NodeMCU-1")) {
+    if (debug) Serial.println("failed to connect and hit timeout");
+    delay(3000);
+    //reset and try again, or maybe put it to deep sleep
+    ESP.restart();
+    delay(5000);
+  } else {
+    if (debug) Serial.println("Connection established");
+  }
+
+  WiFi.setAutoReconnect(true);
+  WiFi.persistent(true);
+
+  //GPS
+  SerialGPS.begin(9600);
+  while (!SerialGPS) {;}
+  if (debug) Serial.println("GPS connected");
+}
+
+
+String myInput  = "";
+StaticJsonDocument<2000> doc;
+        
+void loop() {
+  wifi_reconnect();
+  mqttClient.poll();
+  unsigned long currentMillis = millis();
+
+  while (SerialGPS.available() > 0) {
+      gps.encode(SerialGPS.read());
+      
+      if (gps.location.isUpdated()) {
+        myInput  = "";
+        
+        doc["devId"] = myMacAdr;
+        doc["latitude"] = gps.location.lat();
+        doc["longitude"] = gps.location.lng();
+        doc["speed"] = gps.speed.kmph();
+
+        doc["tstamp"] = String(gps.date.year())+"-" + String(gps.date.month()) + "-" + String(gps.date.day())
+        + " " + String(gps.time.hour()+2) + ":" + String(gps.time.minute()) + ":" + String(gps.time.second());
+        
+        
+        serializeJson(doc, myInput);
+      }
+  }
+
+  
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time a message was sent
+    previousMillis = currentMillis;
+
+    if (myInput=="") {
+      Serial.println("No data");
+    } else {
+      if (debug) {
+        Serial.print("Sending message to topic: ");
+        Serial.print(topic_str + "/json");
+        Serial.print(">>>>>>>>>>>> ");
+        Serial.print(myInput);
+        Serial.println("<<<<<");
+      }
+      mqttClient.beginMessage(topic_str + "/json");
+      mqttClient.print(myInput);
+      mqttClient.endMessage();
+    
+      //in addition plain values without json
+      
+      mqttClient.beginMessage(topic_str + "/"+ myMacAdr + "/value");
+      String speedStr = doc["speed"];
+      mqttClient.print(speedStr);
+      mqttClient.endMessage();
+
+      mqttClient.beginMessage(topic_str + "/"+ myMacAdr + "/latitude");
+      String latStr = doc["latitude"];
+      mqttClient.print(latStr);
+      mqttClient.endMessage();
+      
+      mqttClient.beginMessage(topic_str + "/"+ myMacAdr + "/longitude");
+      String lngStr = doc["longitude"];
+      mqttClient.print(lngStr);
+      mqttClient.endMessage();
+    }
+  }
+ 
+
+}
+
---
+++ NodeMCU_arduino_ide_speedoMeter/README.md
@@ -0,0 +1,9 @@
+# using NodeMCU and Neo6GPS
+Hardware:
+
+NodeMCU-ESP8266-ESP-12E-Development<br>
+https://www.amazon.de/gp/product/B09YLYD2GT/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1
+
+Software:
+TinyGPS
+WiFiManager
