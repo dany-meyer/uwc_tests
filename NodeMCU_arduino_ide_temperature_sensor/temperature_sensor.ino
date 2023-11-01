d8a76e48cbe724a4b108d1e8b553b2e15bbcf7a2
AndileMasela <61992639+Andile-coder@users.noreply.github.com>
2023-11-01 23:11:23 +0200

    Update temperature_sensor.ino
    Code to read in the temperature

--- NodeMCU_arduino_ide_temperature_sensor/temperature_sensor.ino
+++ NodeMCU_arduino_ide_temperature_sensor/temperature_sensor.ino
@@ -1,1 +1,15 @@
+const int temperaturePin = A0; //The middle pin in the sensor connects to A0
+// Note : using LM35 temperature sensor 
+void setup() {
+  pinMode(temperaturePin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int temp = analogRead(temperaturePin);
+  float volts = (temp *500 )/ 1023;  // convert to celcius
+  float celcius = volts; 
+  Serial.println(celcius);
+  delay(1000);
+}
 
